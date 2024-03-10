11acce98b429f9d19ff8b3b8e9de4e818db74abc
vuqt06 <vuqt06@gmail.com>
2024-03-10 18:22:42 -0400

    setup database

---
+++ app/(setup)/page.tsx
@@ -0,0 +1,31 @@
+import { initialProfile } from '@/lib/initial-profile';
+import { db } from '@/lib/db';
+import { redirect } from 'next/navigation';
+
+const SetupPage = async () => {
+    const profile = await initialProfile();
+
+    const server = await db.server.findFirst({
+        where: {
+           members: {
+                some: {
+                     profileId: profile.id,
+                },
+              },
+           }
+    });
+
+    if (server) {
+        return redirect(`/servers/${server.id}`);
+    }
+
+
+    return (
+        <div>
+        <h1>Setup</h1>
+        <p>Setup your account</p>
+        </div>
+    );
+}
+
+export default SetupPage;
