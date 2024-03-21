2528519a44fd6d170819d6470f68581cf21b584e
vuqt06 <vuqt06@gmail.com>
2024-03-21 14:46:20 -0400

    Implement editing serevr

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -4,6 +4,7 @@ import { CreateServerModal } from "@/components/modals/create-server-modal";
 import { InviteModal } from "../modals/invite-modal";
 
 import { useEffect, useState } from "react";
+import { EditServerModal } from "../modals/edit-server-modal";
 
 
 export const ModalProvider = () => {
@@ -21,6 +22,7 @@ export const ModalProvider = () => {
         <>
             <CreateServerModal />
             <InviteModal />
+            <EditServerModal />
         </>
     );
 };
